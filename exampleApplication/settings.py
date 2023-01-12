f2b66b033edadb5448d72b48365f0cf8246c32e6
Junho-06 <k34920032@gmail.com>
2023-01-12 22:42:14 +0900

    Health check

--- exampleApplication/settings.py
+++ exampleApplication/settings.py
@@ -57,7 +57,21 @@ INSTALLED_APPS = [
     'exampleApplication'
 ]
 
+from django.http import HttpResponse
+
+
+class HealthCheckMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.path == '/health':
+            return HttpResponse('ok')
+        return self.get_response(request)
+
 MIDDLEWARE = [
+    'config.middleware.HealthCheckMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
